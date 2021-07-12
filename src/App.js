8608565b798a8bec3f279e479d9496484c1bc525
mamadykonte <contactmamadykonte@gmail.com>
2021-07-12 18:39:42 +0200

    modif h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <h1>Hello world</h1>
       </header>
     </div>
   );
