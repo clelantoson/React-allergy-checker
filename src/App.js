af5fbd0a2ad1b1f6cee7dce20479a87a01e1c6b1
mamadykonte <contactmamadykonte@gmail.com>
2021-07-13 11:38:08 +0200

    delete button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Button from '@material-ui/core/Button';
 import Login from './components/Login'
 
 function App() {
@@ -8,9 +7,6 @@ function App() {
       <header className="App-header">
         <h1>Hello world</h1>
         <Login></Login>
-        <Button variant="contained" color="primary">
-         Test Material UI
-        </Button>
       </header>
     </div>
   );
