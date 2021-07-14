5259939c54671a3b01f244ebe4dd8ee278b18cc8
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-07-14 11:06:35 +0200

    expand doesnt work on the conponent, issue with the warning: findDOMNode is deprecated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Routes from './router/Routes';
 import './App.css';
 import Login from './components/Login'
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello world</h1>
-        <Login></Login>
       </header>
       <BrowserRouter>
           <Routes/>
--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Route, Switch} from 'react-router-dom';
+import Product from '../components/Product'
+import Login from '../components/Login'
 
 function Routes(props) {
     return (
         <Switch>
-            <Route exact path="/" />
+             <Route exact path="/" />
+            <Route exact path="/login" component={Login}/>
+            <Route exact path="/product" component={Product}/>
         </Switch>
     )
 }
