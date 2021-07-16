3add8595385f5b27da26cb118fea5fb42c60ce0f
dom <dom@example.com>
2021-07-16 14:15:16 +0200

    create component header, added logo and a link

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Routes from "./router/Routes";
 import Navigation from "./components/Navigation";
 
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header 
+          imgSrc="https://picsum.photos/200/300"
+        />
         <Routes />
         <Navigation />
       </BrowserRouter>
