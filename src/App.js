98d53badefe3a51b684c0352209c088266dab107
mamadykonte <contactmamadykonte@gmail.com>
2021-07-19 02:16:06 +0200

    created section allergens personel info

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./router/Routes";
 import Navigation from "./components/Navigation";
-
+// add css
+import 'normalize.css';
 import "./App.css";
 
 function App() {
--- src/router/Routes.js
+++ src/router/Routes.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "../components/Login";
 import Home from "../components/Home";
+import Profile from "../components/Profile";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      {/* <Route path="/profile" component={Profile}/>
-            <Route path="/product" component={Product}/> */}
+      <Route path="/profile" component={Profile}/>
+      {/*  <Route path="/product" component={Product}/> */}
       {/* <Route path="/recents" component={recents}/> */}
       <Route path="/login" component={Login} />
       {/* <Route path="/" component={}/> */}
