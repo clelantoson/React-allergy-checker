aa1df5c804edb98003207b797634b33fc5f4eddf
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-07-26 17:06:11 +0200

    fetch product working with single url, need to implement params

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "../components/Login";
 import Home from "../components/Home";
-import Product from "../components/Product";
+import ProductDetail from "../components/ProductDetail";
 import FetchProduct from "../components/FetchProduct";
 
 const Routes = () => {
@@ -10,7 +10,7 @@ const Routes = () => {
     <Switch>
       <Route exact path="/" component={Home} />
       {/* <Route path="/profile" component={Profile}/> */}
-      <Route path="/product" component={Product} />
+      <Route path="/product/:id" component={ProductDetail} />
       {/* <Route path="/recents" component={recents}/> */}
       <Route path="/login" component={Login} />
       <Route path="/fetch" component={FetchProduct} />
