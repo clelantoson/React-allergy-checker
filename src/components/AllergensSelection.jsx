d163dd62008631614beaed869e2a579880e58593
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-08-10 12:37:27 +0200

    added array to refacto components chips

--- src/components/AllergensSelection.jsx
+++ src/components/AllergensSelection.jsx
@@ -5,98 +5,118 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    // position: "fixed",
-    // bottom: 0,
-    // width: "100%",
     textAlign: "center",
   },
   chip: {
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.info,
+  },
+  styledChips: {
+    margin: "0.1rem",
   },
 }));
 
 const AllergensSelection = () => {
   const classes = useStyles();
+  const allergens = [
+    "Milk",
+    "Gluten",
+    "Soybeans",
+    "Peanuts",
+    "Nuts",
+    "Eggs",
+    "Mustard",
+    "Molluscs",
+    "Celery",
+    "Fish",
+  ];
+
   return (
-    <>
-      <div className={classes.root}>
-        <h1>Which allergens do you want to avoid ?</h1>
-        <h2>Allergens or traces</h2>
-        <div className={classes.chip}>
-          <Chip
-            color="secondary"
-            size="small"
-            label="Milk"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Gluten"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Soybeans"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Peanuts"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Nuts"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Eggs"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Mustard"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Molluscs"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Celery"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-          <Chip
-            color="secondary"
-            size="small"
-            label="Fish"
-            deleteIcon={<DoneIcon />}
-            // onDelete={handleDelete}
-          />
-        </div>
+    <div className={classes.root}>
+      <h1>Which allergens do you want to avoid ?</h1>
+      <h2>Allergens or traces</h2>
+      <div className={classes.chip}>
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Milk"
+          deleteIcon={<DoneIcon />}
+          //onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Gluten"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Soybeans"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Peanuts"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Nuts"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Eggs"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Mustard"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Molluscs"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Celery"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
+        <Chip
+          className={classes.styledChips}
+          color="primary"
+          size="small"
+          label="Fish"
+          deleteIcon={<DoneIcon />}
+          // onDelete={handleDelete}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
