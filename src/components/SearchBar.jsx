31135ad2bb1c08152d5116ada013a6202c0c7588
Dlyf <domilung@gmail.com>
2021-08-10 23:29:07 +0200

    before component ProductList added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from "react";
 import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core";
+import {
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  makeStyles,
+} from "@material-ui/core";
+// import Divider from '@material-ui/core/Divider';
+// import StarIcon from '@material-ui/icons/Star';
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import Paper from "@material-ui/core/Paper";
+// import { shadows } from '@material-ui/system';
 import axios from "axios";
+
 
 const useStyles = makeStyles({
   root: {
+    width: "100%",
+    top: "100",
     display: "flex",
+    flexDirection: "column",
     justifyContent: "center",
   },
   searchBar: {
     width: "50%",
+    margin: "0 auto",
+    display: "block",
   },
+  list: {
+    width: "100%",
+    height: "700px",
+    marginBottom: "100px",
+  },
+  listItem: {
+    width: "100%",
+    margin: "20px 0 0 0",
+    padding: "0 100px 90px 100px",
+    // border: "1px solid black",
+    borderRadius: "20px",
+    // boxShadow: "0px 0.630645px 1.26129px", rgba("97, 97, 97, 0.2")
+  },
+  paper: {
+    width: "100%"
+  }
 });
 
 const SearchBar = () => {
@@ -52,16 +89,53 @@ const SearchBar = () => {
         type="search"
         variant="outlined"
         className={classes.searchBar}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="end">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
       />
-      <ul>
-        {console.log("products to display", products)}
-        {/* {products.map((product) => {
+      <List className={classes.list}>
+        {console.log("products to display2", products)}
+        {products.map((product) => (
+          <ListItem
+            className={classes.listItem}
+            key={product.id}
+            alignItems="flex-start"
+          >
+            <Paper className={classes.paper} elevation={19}>
+              <ListItemAvatar>
+                <Avatar src={product.image_front_url} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={product.generic_name}
+                secondary={product.id}
+              />
+              <IconButton
+                type="submit"
+                className={classes.iconButton}
+                aria-label="search"
+              >
+              <CheckCircleOutlineIcon />
+              </IconButton>
+            </Paper>
+          </ListItem>
+        ))}
+      </List>
+      {/* <ul> */}
+      {/* {console.log("products to display", products)} */}
+      {/* {products.map((product) => {
           return <li key={product.id}>{product.generic_name}</li>;
         })} */}
-        {products.map((product) => (
-          <li key={product.id}>{product.generic_name}</li>
-        ))}
-      </ul>
+      {/* {products.map((product) => ( */}
+
+      {/* <li key={product.id}>{product.generic_name}</li> */}
+
+      {/* ))} */}
+      {/* <ProductList products={products} /> */}
+      {/* </ul> */}
     </div>
   );
 };
