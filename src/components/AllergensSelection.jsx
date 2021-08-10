f2864a8f0792b3d0382bb785d4a3cc364390003d
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-08-10 15:44:26 +0200

    created toggle on allergens clicked

--- src/components/AllergensSelection.jsx
+++ src/components/AllergensSelection.jsx
@@ -1,34 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Chip from "@material-ui/core/Chip";
 import DoneIcon from "@material-ui/icons/Done";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     textAlign: "center",
   },
-  chip: {
-    color: theme.palette.text.info,
-  },
-  styledChips: {
+  //   chip: {
+  //     color: theme.palette.text.info,
+  //   },
+  selectedChip: {
     margin: "0.1rem",
+  },
+  notSelectedChip: {
+    margin: "0.1rem",
+    opacity: "0.3",
   },
 }));
 
 const AllergensSelection = () => {
   const classes = useStyles();
-  const allergens = [
-    "Milk",
-    "Gluten",
-    "Soybeans",
-    "Peanuts",
-    "Nuts",
-    "Eggs",
-    "Mustard",
-    "Molluscs",
-    "Celery",
-    "Fish",
-  ];
+  const [allergens, setAllergens] = useState([
+    { name: "Milk", selected: false },
+    { name: "Gluten", selected: false },
+    { name: "Soybeans", selected: false },
+    { name: "Peanuts", selected: false },
+    { name: "Nuts", selected: false },
+    { name: "Eggs", selected: false },
+    { name: "Mustard", selected: false },
+    { name: "Molluscs", selected: false },
+    { name: "Celery", selected: false },
+    { name: "Fish", selected: false },
+  ]);
+
+  //   // eslint-disable-next-line no-debugger
+  //   debugger;
+
+  const handleSelectAllergen = (event) => {
+    console.log("event", event.target.innerText);
+    const newAllergens = [...allergens];
+    const foundAllergen = newAllergens.find(
+      (allergen) => allergen.name === event.target.innerText
+    );
+    foundAllergen.selected = !foundAllergen.selected;
+    setAllergens(newAllergens);
+  };
 
   return (
     <div className={classes.root}>
@@ -38,11 +55,14 @@ const AllergensSelection = () => {
         {allergens.map((allergen, index) => (
           <Chip
             key={index}
-            className={classes.styledChips}
+            className={
+              allergen.selected ? classes.selectedChip : classes.notSelectedChip
+            }
             color="primary"
             size="small"
-            label={allergen}
+            label={allergen.name}
             deleteIcon={<DoneIcon />}
+            onClick={handleSelectAllergen}
             //onDelete={handleDelete}
           />
         ))}
