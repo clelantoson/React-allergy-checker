2e4e5fb3688050fd524f417d43153f85c73a4789
mamadykonte <contactmamadykonte@gmail.com>
2021-08-20 14:08:50 +0200

    create api file

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -12,31 +12,43 @@ import {
   USER_UPDATE_SUCCESS,
 } from "../constants/userConstants";
 
-  export const loginActions = (email, password) => async (dispatch) => {
-      try {
-        dispatch({ type: USER_LOGIN_REQUEST });
-        const { data } = await axios.post("http://localhost:5000/user/login", {
-          email,
-          password,
-        });
-        dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-        localStorage.setItem("userInfo", JSON.stringify(data));
-      } catch (error) {
-        dispatch({ type: USER_LOGIN_FAIL, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
-      }
+export const loginActions = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+    const { data } = await axios.post("http://localhost:5000/user/login", {
+      email,
+      password,
+    });
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
+};
 
-  export const logoutActions = () => async (dispatch) => {
-    try {
-      localStorage.removeItem("userInfo");
-      dispatch({ type: USER_LOGOUT });
-    } catch (error) {
-      dispatch({ type: USER_LOGOUT, payload: error.response && error.response.data.message ? error.response.data.message : error.message });
-    }
-    
-  };
+export const logoutActions = () => async (dispatch) => {
+  try {
+    localStorage.removeItem("userInfo");
+    dispatch({ type: USER_LOGOUT });
+  } catch (error) {
+    dispatch({
+      type: USER_LOGOUT,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
-  export const registerActions = (firstName, lastName, email, password, pic) => async (dispatch) => {
+export const registerActions =
+  (firstName, lastName, email, password, pic) => async (dispatch) => {
     try {
       dispatch({ type: USER_REGISTER_REQUEST });
 
@@ -62,42 +74,41 @@ import {
             : error.message,
       });
     }
-  }
-  
-  export const updateProfile = (user) => async (dispatch, getState) => {
-    try {
-      dispatch({ type: USER_UPDATE_REQUEST });
-
-      const {
-        userLogin: { userInfo },
-      } = getState();
-
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${userInfo.token}`,
-        },
-      };
-
-      const { data } = await axios.post(
-        "http://localhost:5000/user/profile",
-        user,
-        config
-      );
-
-      dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
-
-      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-
-      localStorage.setItem("userInfo", JSON.stringify(data));
-    } catch (error) {
-      dispatch({
-        type: USER_UPDATE_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
   };
-  
+
+export const updateProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post(
+      "http://localhost:5000/user/profile",
+      user,
+      config
+    );
+
+    dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
---
+++ src/components/profile/userProfile.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Avatar,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+
+ 
+} from "@material-ui/core";
+
+
+
+
+
+import img from "../../img/profile-bg.jpg";
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: theme.spacing(2),
+  },
+  width: {
+    maxWidth: "100%",
+    margin: "0 auto",
+  },
+ 
+}));
+
+
+
+const UserProfile = ({userInfo}) => {
+
+  const classes = useStyles();
+  // const [firstName, setFirstName] = useState(userInfo?.name.split(" ")[0]);
+  // const [lastName, setLastName] = useState(userInfo?.name.split(" ")[1]);
+
+  return (
+    <div className={classes.width}>
+      <Card className={classes.root}>
+        <Avatar
+          alt={userInfo?.name}
+          src={
+            userInfo?.pic
+              ? userInfo?.pic
+              : "https://images.unsplash.com/photo-1626193759855-4f03fc744287?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
+          }
+        />
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image={img}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h4" component="h2">
+              {userInfo?.name.split(" ")[0]} {userInfo?.name.split(" ")[1]}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+     
+    </div>
+  );
+};
+
+export default UserProfile;
