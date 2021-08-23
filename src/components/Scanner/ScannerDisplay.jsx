1634e58d50e885b44d015b0abb84406b3ba03509
Camille Le Lan-Toson <clelantoson@hotmail.fr>
2021-08-23 16:30:18 +0200

    centered elements

--- src/components/Scanner/ScannerDisplay.jsx
+++ src/components/Scanner/ScannerDisplay.jsx
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import Scanner from "./Scanner";
 import Button from "@material-ui/core/Button";
+import "./ScannerDisplay.css";
 
 const ScannerDisplay = () => {
   const [scanning, setScanning] = useState(false);
@@ -26,7 +27,7 @@ const ScannerDisplay = () => {
   }, [productEAN]);
 
   return (
-    <div>
+    <div className="scannerContainer">
       <Button
         onClick={() => setScanning(!scanning)}
         variant="contained"
