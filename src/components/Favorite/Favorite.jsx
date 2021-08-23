a1700104f44a51e71062c2ae3dcbd45b741b0f20
dom <dom@example.com>
2021-08-23 17:20:13 +0200

    Begin Favorite Component

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,54 +1,10 @@
-import { List, makeStyles } from "@material-ui/core";
 import React from "react";
-import ProductItem from "../Product/ProductItem";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    width: "100%",
-    height: "100%",
-    overflowY: "auto",
-  },
-  fullWidth: { width: "100%" },
-  searchBar: {
-    width: "50%",
-  },
-  textField: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: "1rem",
-  },
-  list: {},
-  centerItem: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-  },
-  circleIcon: {
-    marginRight: "4px",
-  },
-  listItemAvatar: {
-    display: "flex",
-    justifyContent: "center",
-  },
-  listItem: {
-    padding: "0 1rem 0 1rem",
-    borderRadius: "20px",
-    marginBottom: "4px",
-  },
-  paper: {
-    width: "100%",
-  },
-}));
-
-const Favorite = (products) => {
-  const classes = useStyles();
+const Favorite = () => {
     return (
-      <List className={classes.list}>
-      {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
-    </List>
+      <div>
+        <h1>Hello world</h1>
+      </div>
     );
   };
 
