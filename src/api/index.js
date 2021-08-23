a322afc9e698cfe408ab9d39edb5bfc229ba74d5
mamadykonte <contactmamadykonte@gmail.com>
2021-08-23 14:38:09 +0200

    change url api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://api-food-checker.herokuapp.com/",
 });
 
 API.interceptors.request.use((req) => {
